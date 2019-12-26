2d27dfb56fbe01430a0bb3c12b14abbc6043efe0
jgsneves <jgsn.adv@gmail.com>
2019-12-26 18:13:56 -0300

    Updates

---
+++ precache-manifest.fe97104c6f86a2d2daaf1509d0067890.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf5d985c82f6e102aa226fd52028bbf",
+    "url": "/starwarsapp/index.html"
+  },
+  {
+    "revision": "2102eefd2077dd97d216",
+    "url": "/starwarsapp/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "059e5cd127cc954b29b7",
+    "url": "/starwarsapp/static/css/main.bd941da3.chunk.css"
+  },
+  {
+    "revision": "2102eefd2077dd97d216",
+    "url": "/starwarsapp/static/js/2.2d6e6e8c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwarsapp/static/js/2.2d6e6e8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "059e5cd127cc954b29b7",
+    "url": "/starwarsapp/static/js/main.eabe5f27.chunk.js"
+  },
+  {
+    "revision": "497e5ba2f16f64a65d75",
+    "url": "/starwarsapp/static/js/runtime-main.62b643c4.js"
+  },
+  {
+    "revision": "16f9ed321c283528b972e0e1a718f3cd",
+    "url": "/starwarsapp/static/media/640787.16f9ed32.jpg"
+  },
+  {
+    "revision": "68359915487678bb4f821e2d820c0fd5",
+    "url": "/starwarsapp/static/media/StarJediHollow.68359915.ttf"
+  },
+  {
+    "revision": "e60047af3b913094d0e117b64a2c21e0",
+    "url": "/starwarsapp/static/media/StarJediHollow.e60047af.woff2"
+  },
+  {
+    "revision": "ef7ce9786eb6a0cac4db09a71cbc99ac",
+    "url": "/starwarsapp/static/media/StarJediHollow.ef7ce978.eot"
+  },
+  {
+    "revision": "f6bc396b76cb5df0287e14d64a199d3c",
+    "url": "/starwarsapp/static/media/StarJediHollow.f6bc396b.woff"
+  }
+]);
