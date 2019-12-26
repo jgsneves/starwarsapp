8650c39305b50c7ac7f126b9c0a088e56592145a
jgsneves <jgsn.adv@gmail.com>
2019-12-26 17:52:10 -0300

    commit

--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,15 @@ import React from 'react';
 const Card = ({card}) => {
     return (
         <div className='div2'>
-            <p>{card.title}</p>
+            <h2>{card.title}</h2>
             {card.infos.map(info => (
                 <div key={info.title}>
-                    {info.title} - {info.value}
+                    <div className='titulo'>
+                        {info.title}:
+                    </div>
+                    <div className='texto'>
+                        {info.value} 
+                    </div>
                 </div>
             ))}
         </div>
--- src/getCards.js
+++ src/getCards.js
@@ -1,17 +1,5 @@
 export function getCards(genero, itens) {
     switch (genero) {
-        case "planets":
-            return itens.map(item => ({
-                title: item.name,
-                infos: [{
-                    title: "Diametro",
-                    value: item.diameter
-                },
-                {
-                    title: "Período de Rotação:",
-                    value: item.rotation_period
-                }]
-            }))
         case "films":
             return itens.map(item => ({
                 title: item.title,
@@ -22,6 +10,234 @@ export function getCards(genero, itens) {
                 {
                     title: "Sinopse",
                     value: item.opening_crawl
+                },
+                {
+                    title: "Diretor",
+                    value: item.director 
+                },
+                {
+                    title: "Produtor",
+                    value: item.producer  
+                },
+                {
+                    title: "Ano de lançamento",
+                    value: item.release_date   
+                },
+                ]
+            }))
+        case "people":
+            return itens.map(item => ({
+                title: item.name,
+                infos: [{
+                    title: "Ano de nascimento",
+                    value: item.birth_year 
+                },
+                {
+                    title: "Cor dos olhos",
+                    value: item.eye_color
+                },
+                {
+                    title: "Sexo",
+                    value: item.gender 
+                },
+                {
+                    title: "Altura",
+                    value: item.height 
+                },
+                {
+                    title: "Peso",
+                    value: item.mass  
+                },
+                {
+                    title: "Naturalidade",
+                    value: item.homeworld   
+                },
+                {
+                    title: "Filmes que participa",
+                    value: item.films    
+                },
+                {
+                    title: "Espécies",
+                    value: item.species     
+                },
+                {
+                    title: "Starship",
+                    value: item.starships      
+                },
+                {
+                    title: "Veículos",
+                    value: item.vehicles       
+                }]
+            }))
+        case "planets":
+            return itens.map(item => ({
+                title: item.name,
+                infos: [{
+                    title: "Diametro",
+                    value: item.diameter
+                },
+                {
+                    title: "Período de Rotação",
+                    value: item.rotation_period
+                },
+                {
+                    title: "Período de Órbita",
+                    value: item.orbital_period 
+                },
+                {
+                    title: "Gravidade",
+                    value: item.gravity  
+                },
+                {
+                    title: "População",
+                    value: item.population   
+                },
+                {
+                    title: "Clima",
+                    value: item.climate    
+                },
+                {
+                    title: "Terreno",
+                    value: item.terrain     
+                },
+                {
+                    title: "Supercífie D'água",
+                    value: item.surface_water      
+                },
+                {
+                    title: "Residentes",
+                    value: item.residents       
+                }
+                ]
+            }))
+        case "species":
+            return itens.map(item => ({
+                title: item.name,
+                infos: [{
+                    title: "Classificação",
+                    value: item.classification 
+                },
+                {
+                    title: "Designação",
+                    value: item.designation 
+                },
+                {
+                    title: "Média de altura",
+                    value: item.average_height  
+                },
+                {
+                    title: "Media de idade",
+                    value: item.average_lifespan   
+                },
+                {
+                    title: "Cor dos olhos",
+                    value: item.eye_colors    
+                },
+                {
+                    title: "Cor da pele",
+                    value: item.skin_colors     
+                },
+                {
+                    title: "Linguagem",
+                    value: item.language      
+                },
+                {
+                    title: "Planeta de origem",
+                    value: item.homeworld       
+                },
+                {
+                    title: "Pessoas",
+                    value: item.people        
+                }
+                ]
+            }))
+        case "starships":
+            return itens.map(item => ({
+                title: item.name,
+                infos: [{
+                    title: "Modelo",
+                    value: item.model  
+                },
+                {
+                    title: "Classe",
+                    value: item.starship_class  
+                },
+                {
+                    title: "Marca",
+                    value: item.manufacturer   
+                },
+                {
+                    title: "Preço",
+                    value: item.cost_in_credits    
+                },
+                {
+                    title: "Largura",
+                    value: item.length     
+                },
+                {
+                    title: "Tripulação",
+                    value: item.crew      
+                },
+                {
+                    title: "Passageiros",
+                    value: item.passengers       
+                },
+                {
+                    title: "Velocidade Máxima",
+                    value: item.max_atmosphering_speed        
+                },
+                {
+                    title: "Limite de peso/transporte",
+                    value: item.cargo_capacity         
+                },
+                {
+                    title: "Quem já pilotou",
+                    value: item.pilots          
+                }
+                ]
+            }))
+        case "vehicles":
+            return itens.map(item => ({
+                title: item.name,
+                infos: [{
+                    title: "Modelo",
+                    value: item.model  
+                },
+                {
+                    title: "Classe",
+                    value: item.vehicle_class   
+                },
+                {
+                    title: "Marca",
+                    value: item.manufacturer   
+                },
+                {
+                    title: "Preço",
+                    value: item.cost_in_credits    
+                },
+                {
+                    title: "Largura",
+                    value: item.length     
+                },
+                {
+                    title: "Tripulação",
+                    value: item.crew      
+                },
+                {
+                    title: "Passageiros",
+                    value: item.passengers       
+                },
+                {
+                    title: "Velocidade Máxima",
+                    value: item.max_atmosphering_speed        
+                },
+                {
+                    title: "Limite de peso/transporte",
+                    value: item.cargo_capacity         
+                },
+                {
+                    title: "Quem já pilotou",
+                    value: item.pilots          
                 }
                 ]
             }))
